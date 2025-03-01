519adface45aaa1119dafb44cca9c9636455dc17
JoeM. <75545397+mabayajoe@users.noreply.github.com>
2025-02-28 20:01:52 -0700

    Updated naming convention on a subnet in  ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public" {
   cidr_block               = "10.0.1.0/24"
   map_public_ip_on_launch  = true
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet1"
   }
 }
 
