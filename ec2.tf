5c43a7f669d5f1236bef9cfb2f27eac24ea79773
nbhilda <ndehilda00@gmail.com>
2025-05-18 03:24:26 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,26 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+terraform {
+  backend "s3" {
+    bucket = "awsclasses2024"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+  }
+
+
+variable "access_key" {
+ type : string
+}
+
+variable "secret_key" {
+ type : string
 }
 
 #VPC BLOCK
