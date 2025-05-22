3eb63a5708b206bb677a09ec0bdb1955f60e13c5
hilda <ndehilda00@gmail.com>
2025-05-22 02:37:23 -0500

    passing access and secret keys

--- ec2.tf
+++ ec2.tf
@@ -29,8 +29,8 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-internet gateway for march
-resource "aws_internet_gateway" "gw" {
+internet gateway for march {
+resource "aws_internet_gateway" "gw" 
   vpc_id = aws_vpc.main.id.main
   tags = {
     Name = "main"
@@ -48,8 +48,8 @@ resource "aws_subnet" "public" {
 }
 
 
-private subnet for march
-resource "aws_subnet" "private" {
+private subnet for march {
+resource "aws_subnet" "private" 
   vpc_id     = aws_vpc.main.id.main
   cidr_block = "10.0.2.0/24"
   tags = {
